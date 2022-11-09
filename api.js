d104ad2bcaf4d7e7bd6668173ee65354bf92a1aa
Trenton Kress <trenton.kress@gmail.com>
2022-11-09 11:24:28 -0800

    Refactor and add another river source

--- api.js
+++ api.js
@@ -5,7 +5,61 @@ const { Parser } = require('xml2js');
 let router = express.Router();
 
 router.get('/data', async (req, res, next) => {
-    let xmlRes = await axios.get('https://water.weather.gov/ahps2/hydrograph_to_xml.php?gage=glbw1&output=xml');
+    let rivers = {};
+    rivers.skykomish = await getRiverData('https://water.weather.gov/ahps2/hydrograph_to_xml.php?gage=glbw1&output=xml');
+    rivers.skykomish.annotations = {
+        getABoat: {
+            type: 'line',
+            borderColor: 'rgb(200, 0, 0)',
+            borderDash: [6, 6],
+            borderDashOffset: 0,
+            borderWidth: 3,
+            label: {
+                position: 'end',
+                display: true,
+                backgroundColor: 'rgb(128, 0, 0)',
+                content: 'GET A BOAT'
+            },
+            scaleID: 'y',
+            value: 13
+        },
+        waders: {
+            type: 'line',
+            borderColor: 'rgb(234, 134, 4)',
+            borderDash: [6, 6],
+            borderDashOffset: 0,
+            borderWidth: 3,
+            label: {
+                position: 'end',
+                display: true,
+                backgroundColor: 'rgb(234, 134, 4)',
+                content: 'Cedar/Intake only unless ballsy'
+            },
+            scaleID: 'y',
+            value: 9
+        },
+        waders2: {
+            type: 'line',
+            borderColor: 'rgb(47, 181, 3)',
+            borderDash: [6, 6],
+            borderDashOffset: 0,
+            borderWidth: 3,
+            label: {
+                position: 'end',
+                display: true,
+                backgroundColor: 'rgb(47, 181, 3)',
+                content: 'Perfect. All holes accessible.'
+            },
+            scaleID: 'y',
+            value: 7
+        }
+    };
+    rivers.snohomish = await getRiverData('https://water.weather.gov/ahps2/hydrograph_to_xml.php?gage=mrow1&output=xml');
+    res.json(rivers);
+});
+
+async function getRiverData(url) {
+    let xmlRes = await axios.get(url);
     if (!xmlRes.data)
         return res.json({ error: 'Error parsing data from source.' });
     let parser = new Parser({ explicitArray: false });
@@ -14,7 +68,7 @@ router.get('/data', async (req, res, next) => {
         return res.json({ error: 'Observed data is missing.' });
     let dataPoints = [];
     let observed = riverData.site.observed.datum;
-    for (let i = 0;i < observed.length;i++) {
+    for (let i = 0; i < observed.length; i++) {
         if (!observed[i])
             continue;
         dataPoints.push({
@@ -26,18 +80,76 @@ router.get('/data', async (req, res, next) => {
         });
     }
     let forecast = riverData.site.forecast.datum;
-    for (let i = 0;i < forecast.length;i++) {
+    for (let i = 0; i < forecast.length; i++) {
         if (!forecast[i])
             continue;
-            dataPoints.push({
-                prediction: true,
-                timeString: moment(forecast[i].valid._).utc().local().format('DD/MM/YYYY hh:mm:ss'),
-                timestamp: Number(moment(forecast[i].valid._).utc().local().valueOf()),
-                level: forecast[i].primary._,
-                flow: forecast[i].secondary._
-            });
+        dataPoints.push({
+            prediction: true,
+            timeString: moment(forecast[i].valid._).utc().local().format('DD/MM/YYYY hh:mm:ss'),
+            timestamp: Number(moment(forecast[i].valid._).utc().local().valueOf()),
+            level: forecast[i].primary._,
+            flow: forecast[i].secondary._
+        });
     }
-    res.json(dataPoints);
-});
+    let data = {
+        labels: ['Level (feet)', 'Flow (kcfs)'],
+        datasets: [{
+            label: 'Level',
+            data: [],
+            backgroundColor: [],
+            borderColor: [],
+            borderWidth: 1,
+            yAxisID: 'y'
+        },
+        {
+            hidden: true,
+            label: 'Flow',
+            data: [],
+            backgroundColor: [],
+            borderColor: [],
+            borderWidth: 1,
+            yAxisID: 'y1'
+        }]
+    };
+    let minTime = Number.MAX_VALUE, maxTime = 0;
+    let minLevel = Number.MAX_VALUE, maxLevel = 0;
+    let minFlow = Number.MAX_VALUE, maxFlow = 0;
+    dataPoints.forEach(point => {
+        if (point.timestamp < minTime)
+            minTime = point.timestamp;
+        if (point.timestamp > maxTime)
+            maxTime = point.timestamp;
+
+        if (point.level < minLevel)
+            minLevel = point.level;
+        if (point.level > maxLevel)
+            maxLevel = point.level;
+
+        if (point.flow < minFlow)
+            minFlow = point.flow;
+        if (point.flow > maxFlow)
+            maxFlow = point.flow;
+
+        data.datasets[0].data.push({
+            x: point.timestamp,
+            y: point.level
+        });
+        data.datasets[0].backgroundColor.push(point.prediction ? 'rgba(0, 189, 252, 0.1)' : 'rgba(0, 120, 160, 0.1)');
+        data.datasets[0].borderColor.push(point.prediction ? 'rgba(0, 189, 252, 0.4)' : 'rgba(0, 120, 160, 0.4)');
+
+        data.datasets[1].data.push({
+            x: point.timestamp,
+            y: point.flow
+        });
+        data.datasets[1].backgroundColor.push(point.prediction ? 'rgba(0, 128, 100, 0.2)' : 'rgba(0, 255, 100, 0.2)');
+        data.datasets[1].borderColor.push(point.prediction ? 'rgba(0, 128, 100, 1)' : 'rgba(0, 255, 100, 1)');
+    });
+    return {
+        data,
+        minTime, maxTime,
+        minFlow, maxFlow,
+        minLevel, maxLevel
+    };
+}
 
 module.exports = router;
--- public/js/rivers.js
+++ public/js/rivers.js
@@ -1,62 +1,20 @@
 //Snohomish river: https://water.weather.gov/ahps2/hydrograph_to_xml.php?gage=mrow1&output=xml
-$.getJSON('/api/data', function(riverData) {
-  const ctx = document.getElementById('chart').getContext('2d');
-  let data = {
-    labels: ['Level (feet)', 'Flow (kcfs)'],
-    datasets: [{
-      label: 'Level',
-      data: [],
-      backgroundColor: [],
-      borderColor: [],
-      borderWidth: 1,
-      yAxisID: 'y'
-    },
-    {
-      hidden: true,
-      label: 'Flow',
-      data: [],
-      backgroundColor: [],
-      borderColor: [],
-      borderWidth: 1,
-      yAxisID: 'y1'
-    }]
-  };
-  let minTime = Number.MAX_VALUE, maxTime = 0;
-  let minLevel = Number.MAX_VALUE, maxLevel = 0;
-  let minFlow = Number.MAX_VALUE, maxFlow = 0;
-  riverData.forEach(point => {
-    if (point.timestamp < minTime)
-      minTime = point.timestamp;
-    if (point.timestamp > maxTime)
-      maxTime = point.timestamp;
+$.getJSON('/api/data', function (rivers) {
+  createRiverChart(rivers.skykomish, 'Wallace/Skykomish');
+  createRiverChart(rivers.snohomish, 'Snohomish');
+});
 
-    if (point.level < minLevel)
-      minLevel = point.level;
-    if (point.level > maxLevel)
-      maxLevel = point.level;
+function createRiverChart(river, riverTitle) {
+  const canvas = document.createElement('canvas');
+  document.getElementById('chart-container').appendChild(canvas);
+  const ctx = canvas.getContext('2d');
+  const chart = new Chart(ctx, generateChartData(river, riverTitle));
+}
 
-    if (point.flow < minFlow)
-      minFlow = point.flow;
-    if (point.flow > maxFlow)
-      maxFlow = point.flow;
-
-    data.datasets[0].data.push({
-      x: point.timestamp,
-      y: point.level
-    });
-    data.datasets[0].backgroundColor.push(point.prediction ? 'rgba(0, 189, 252, 0.1)' : 'rgba(0, 120, 160, 0.1)');
-    data.datasets[0].borderColor.push(point.prediction ? 'rgba(0, 189, 252, 0.4)' : 'rgba(0, 120, 160, 0.4)');
-
-    data.datasets[1].data.push({
-      x: point.timestamp,
-      y: point.flow
-    });
-    data.datasets[1].backgroundColor.push(point.prediction ? 'rgba(0, 128, 100, 0.2)' : 'rgba(0, 255, 100, 0.2)');
-    data.datasets[1].borderColor.push(point.prediction ? 'rgba(0, 128, 100, 1)' : 'rgba(0, 255, 100, 1)');
-  });
-  const chart = new Chart(ctx, {
+function generateChartData(river, riverTitle) {
+  return {
     type: 'line',
-    data,
+    data: river.data,
     options: {
       elements: {
         line: {
@@ -67,7 +25,7 @@ $.getJSON('/api/data', function(riverData) {
       plugins: {
         title: {
           display: true,
-          text: 'Wallace/Skykomish River Level'
+          text: riverTitle
         },
         annotation: {
           annotations: {
@@ -77,58 +35,14 @@ $.getJSON('/api/data', function(riverData) {
               borderWidth: 1,
               display: (ctx) => ctx.chart.isDatasetVisible(0) || ctx.chart.isDatasetVisible(1),
               label: {
-                display: true,
-                content: 'Now',
-                position: 'start'
+                  display: true,
+                  content: 'Now',
+                  position: 'start'
               },
               scaleID: 'x',
               value: Date.now()
             },
-            getABoat: {
-              type: 'line',
-              borderColor: 'rgb(200, 0, 0)',
-              borderDash: [6, 6],
-              borderDashOffset: 0,
-              borderWidth: 3,
-              label: {
-                position: 'end',
-                display: true,
-                backgroundColor: 'rgb(128, 0, 0)',
-                content: (ctx) => 'GET A BOAT'
-              },
-              scaleID: 'y',
-              value: 13
-            },
-            waders: {
-              type: 'line',
-              borderColor: 'rgb(234, 134, 4)',
-              borderDash: [6, 6],
-              borderDashOffset: 0,
-              borderWidth: 3,
-              label: {
-                position: 'end',
-                display: true,
-                backgroundColor: 'rgb(234, 134, 4)',
-                content: (ctx) => 'Cedar/Intake only unless ballsy'
-              },
-              scaleID: 'y',
-              value: 9
-            },
-            waders2: {
-              type: 'line',
-              borderColor: 'rgb(47, 181, 3)',
-              borderDash: [6, 6],
-              borderDashOffset: 0,
-              borderWidth: 3,
-              label: {
-                position: 'end',
-                display: true,
-                backgroundColor: 'rgb(47, 181, 3)',
-                content: (ctx) => 'Perfect. All holes accessible.'
-              },
-              scaleID: 'y',
-              value: 7
-            }
+            ...river.annotations
           }
         }
       },
@@ -141,16 +55,16 @@ $.getJSON('/api/data', function(riverData) {
           type: 'time',
           time: {
             displayFormats: {
-                quarter: 'MMM DD hh:mm'
+              quarter: 'MMM DD hh:mm'
             }
           },
           title: {
             text: 'Time',
             display: true
           },
-          
-          min: minTime,
-          max: maxTime
+
+          min: river.minTime,
+          max: river.maxTime
         },
         y: {
           display: true,
@@ -161,8 +75,8 @@ $.getJSON('/api/data', function(riverData) {
           ticks: {
             callback: (val, i, ticks) => val + ' ft.'
           },
-          suggestedMin: minLevel,
-          suggestedMax: maxLevel
+          suggestedMin: river.minLevel,
+          suggestedMax: river.maxLevel
         },
         y1: {
           display: true,
@@ -174,10 +88,11 @@ $.getJSON('/api/data', function(riverData) {
           ticks: {
             callback: (val, i, ticks) => val + ' kcfs'
           },
-          suggestedMin: minFlow,
-          suggestedMax: maxFlow
+          suggestedMin: river.minFlow,
+          suggestedMax: river.maxFlow
         }
       }
     },
-  });
-});
+  };
+}
+
