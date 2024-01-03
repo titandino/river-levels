e851407e9c23ac1ab5cfb1de78d5f72811fe63cd
Trenton Kress <trenton.kress@gmail.com>
2024-01-03 01:52:56 -0800

    Dockerize project

--- index.js
+++ index.js
@@ -1,26 +1,9 @@
-const config = require('./config');
-
 const path = require('path');
-const fs = require('fs');
-
-const https = require('https');
-
 const express = require('express');
-
 const server = express();
 
 server.use('/', express.static(path.resolve('./public/')));
 
 server.use('/api', require('./api'));
 
-if (config.https) {
-  const httpsServer = https.createServer({
-    key: fs.readFileSync(config.keyPath),
-    cert: fs.readFileSync(config.certPath),
-  }, server);
-
-	server.listen(config.httpPort, '0.0.0.0', () => console.log('HTTP successful on port ' + config.httpPort));
-  httpsServer.listen(config.httpsPort, '0.0.0.0', () => console.log('HTTPS successful on port ' + config.httpsPort));
-} else {
-  server.listen(config.httpPort, '0.0.0.0', () => console.log('HTTP successful on port ' + config.httpPort));
-}
+server.listen(process.env.PORT || 8080, '0.0.0.0', () => console.log('HTTP successful on port ' + process.env.PORT || 8080));
